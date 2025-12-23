38b9697d55e1ac712db5323535afdcb2c43b02fc
bala-amypo <bala@amypo.in>
2025-12-23 04:37:06 +0000

    User commit

--- src/main/java/com/example/demo/dto/ JwtResponse.java
+++ src/main/java/com/example/demo/dto/ JwtResponse.java
@@ -1,12 +1,9 @@
 package com.example.demo.dto;
 
 public class JwtResponse {
-
     private String token;
     private String email;
     private String role;
-
-    public JwtResponse() {}
 
     public JwtResponse(String token, String email, String role) {
         this.token = token;
@@ -15,27 +12,12 @@ public class JwtResponse {
     }
 
     // Getters and setters
-    public String getToken() {
-        return token;
-    }
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
