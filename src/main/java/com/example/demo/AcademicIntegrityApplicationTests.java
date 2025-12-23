bcf9f4abad1ec6caa99750bda3ff4e40bd39682b
bala-amypo <bala@amypo.in>
2025-12-23 06:33:58 +0000

    User commit

--- src/main/java/com/example/demo/AcademicIntegrityApplicationTests.java
+++ src/main/java/com/example/demo/AcademicIntegrityApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcademicIntegrityApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AcademicIntegrityApplication.class, args);
     }
